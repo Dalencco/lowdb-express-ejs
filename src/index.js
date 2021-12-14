c89e72d437a8051c5a026648d5710f3a9b53f0cc
Devincer <93698307+Devincer@users.noreply.github.com>
2021-12-14 18:56:19 -0300

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./server')
-const { createConnection } = require('./database')
-
-createConnection();
-app.set('port', process.env.PORT || 3000);
-
-app.listen(app.set('port'), () => {
-    console.log('Server listo en el puerto', app.get('port'));
-});
